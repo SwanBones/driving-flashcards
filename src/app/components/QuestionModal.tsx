c4e4610e60e3fde591fcfe242fdae9ea42aa9d75
SwanBones <xuan.boone@gmail.com>
2025-12-31 16:24:28 +0100

    colored checkmarks and fuel indicator image fixed

--- src/app/components/QuestionModal.tsx
+++ src/app/components/QuestionModal.tsx
@@ -6,22 +6,22 @@ import QuestionCardItem from "./QuestionCardItem";
 import { useState } from "react";
 
 type Props = {
-  questions: Question[];
-  isOpen: boolean;
-  onCancel: () => void;
-  questionGroup: QuestionGroup | null;
+	questions: Question[];
+	isOpen: boolean;
+	onCancel: () => void;
+	questionGroup: QuestionGroup | null;
 };
 function QuestionModal(props: Props) {
-  const { questions, isOpen = false, onCancel, questionGroup } = props;
-  const [showAnswers, setShowAnswers] = useState<number[]>([]);
-  const { checkQuestions, uncheckQuestions } = useQuestions();
-  const onInternalCancel = () => {
-    setShowAnswers([]);
-    onCancel();
-  };
-  //not used cuz button is disabled
+	const { questions, isOpen = false, onCancel, questionGroup } = props;
+	const [showAnswers, setShowAnswers] = useState<number[]>([]);
+	const { checkQuestions, uncheckQuestions } = useQuestions();
+	const onInternalCancel = () => {
+		setShowAnswers([]);
+		onCancel();
+	};
+	//not used cuz button is disabled
 
-  /*
+	/*
   const handleToggleAnswers = () => {
     {
       if (showAnswers.length === 3) {
@@ -41,43 +41,42 @@ function QuestionModal(props: Props) {
     }
   };
   */
-  const handleQuestionClick = (question: Question) => {
-    if (showAnswers.includes(question.id)) {
-      setShowAnswers((as) => as.filter((a) => a !== question.id));
-    } else {
-      setShowAnswers((as) => [...as, question.id]);
-      checkQuestions([question.id]);
-    }
-  };
+	const handleQuestionClick = (question: Question) => {
+		if (showAnswers.includes(question.id)) {
+			setShowAnswers((as) => as.filter((a) => a !== question.id));
+		} else {
+			setShowAnswers((as) => [...as, question.id]);
+			checkQuestions([question.id]);
+		}
+	};
 
-  return (
-    <Modal
-      open={isOpen}
-      onCancel={onInternalCancel}
-      title={`Groupe de question #${questionGroup?.group_id}`}
-      footer
-      centered
-    >
-      <div className="flex flex-col gap-4 mt-10">
-        {questions.map((question) => {
-          return (
-            <QuestionCardItem
-              key={`question-modal-${question.id}`}
-              question={question}
-              checkMarkClassname={"!text-lime-500"}
-              handleQuestionClick={handleQuestionClick}
-              showAnswers={showAnswers}
-            />
-          );
-        })}
-        {/* <Button onClick={handleToggleAnswers} size="large" className="mt-4">
+	return (
+		<Modal
+			open={isOpen}
+			onCancel={onInternalCancel}
+			title={`Groupe de question #${questionGroup?.group_id}`}
+			footer
+			centered
+		>
+			<div className="flex flex-col gap-4 mt-10">
+				{questions.map((question) => {
+					return (
+						<QuestionCardItem
+							key={`question-modal-${question.id}`}
+							question={question}
+							handleQuestionClick={handleQuestionClick}
+							showAnswers={showAnswers}
+						/>
+					);
+				})}
+				{/* <Button onClick={handleToggleAnswers} size="large" className="mt-4">
           {showAnswers.length === 3
             ? "Cacher les réponses"
             : `Révéler les réponses`}
         </Button> */}
-      </div>
-    </Modal>
-  );
+			</div>
+		</Modal>
+	);
 }
 
 export default QuestionModal;
